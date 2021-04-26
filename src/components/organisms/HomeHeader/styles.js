fe7e48043d81bce6988d8ce5e6348e5bc5a163a3
Vitor Hugo <vitorhugohonorato@hotmail.com>
2021-04-26 02:16:00 -0300

    Adds animation to logo and search bar

--- src/components/organisms/HomeHeader/styles.js
+++ src/components/organisms/HomeHeader/styles.js
@@ -4,10 +4,32 @@ const Logo = styled.img`
   height: auto;
   max-width: 502px;
   width: 70vw;
+  animation: topDown 2s ease;
+  transform-origin: left top;
+  
+  @keyframes topDown{
+    0%{
+      transform: scaleY(0);
+    }
+    100%{
+      transform: scaleY(1);
+    }
+  }
 `;
 
 const StyledSearchBar = styled.div`
   padding-top: 75px;
+  animation: leftRight 2s ease;
+  transform-origin: left top;
+  
+  @keyframes leftRight{
+    0%{
+      transform: scaleX(0);
+    }
+    100%{
+      transform: scaleX(1);
+    }
+  }
 
   @media(max-width: 800px) {
     padding-top: 50px;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 import home from './home';
-import notFound from './notFound';
 
 const routes = [
   ...home,
-  ...notFound,
 ];
 
 export default routes;
